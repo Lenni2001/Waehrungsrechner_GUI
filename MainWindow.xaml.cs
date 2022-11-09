f4a32e9b6e0647901efd19382a35db311311ecbb
Lenni2001 <lennartsturm@gmx.de>
2022-11-09 10:37:43 +0100

    Display Exchange Rate
    This update displays the exchange rate.
    
    Normalized names...

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -42,30 +42,31 @@ namespace GUI_notconsole_HARD
             string[] currency = new string[10] { "EUR", "USD", "JPY", "GBP", "AUD", "CAD", "CHF", "HKD", "KRW", "XAU" };        //Array für Currency Symbole erstellen
             double[] currencytoEUR = new double[10] { 1.00, 1.18, 126.32, 0.9, 1.65, 1.56, 1.08, 9.16, 1400.27, 0.000607 };     //Array für Umrechnungsfaktoren erstellen
 
-            fromCurrency = this.fromcurrency_combobox.SelectedIndex;        //fromcurrency einlesen
-            toCurrency = this.tocurrency_combobox.SelectedIndex;        //tocurrency einlesen
+            fromCurrency = this.combobox_fromcurrency.SelectedIndex;        //fromcurrency einlesen
+            toCurrency = this.combobox_tocurrency.SelectedIndex;        //tocurrency einlesen
             try
             {
-                value = Convert.ToDouble(this.valuetoconvert.Text);         //value einlesen
+                value = Convert.ToDouble(this.textbox_valuetoconvert.Text);         //value einlesen
             }
             catch
             {
-                this.valuetoconvert.Text = "0";         //falls keine eingabe gemacht wurde ist alles 0!
+                this.textbox_valuetoconvert.Text = "0";         //falls keine eingabe gemacht wurde ist alles 0!
                 value = 0;
             }
+
             double outputValue = (value / currencytoEUR[fromCurrency]) * currencytoEUR[toCurrency];         //Umrechnen
-
-            this.Ergebnis_Textbox.Text = outputValue.ToString("0.00") + " " + Convert.ToString(currency[toCurrency]);         //Ergebnis ausgeben
-            this.log_textbox.Text += (valuetoconvert + " " + currency[fromCurrency] + " --> " + outputValue.ToString("0.00") + " " + currency[toCurrency] + Environment.NewLine);       //Ergebnisse Loggen
-
+            double exchange = outputValue / Convert.ToDouble(textbox_valuetoconvert.Text);          //Umrechungsfaktoren in beider Richtungen bestimmen
+            this.textbox_ergebnis.Text = outputValue.ToString("0.00") + " " + Convert.ToString(currency[toCurrency]);         //Ergebnis ausgeben
+            this.log_textbox.Text += (textbox_valuetoconvert + " " + currency[fromCurrency] + " --> " + outputValue.ToString("0.00") + " " + currency[toCurrency] + " with an exchange Rate of: " + currencytoEUR[toCurrency] +  Environment.NewLine);       //Ergebnisse Loggen
+            this.textbox_exchangerate.Text =  exchange.ToString("0.00");            //Umrechnungsfaktor einsetzen
 
         }
         private void swap_button_Click(object sender, RoutedEventArgs e)
         {
 
-            (this.fromcurrency_combobox.SelectedIndex, this.tocurrency_combobox.SelectedIndex) = (this.tocurrency_combobox.SelectedIndex, this.fromcurrency_combobox.SelectedIndex);            //Swap-Button funktion, tauschen der combobox untereinander 
-            (this.valuetoconvert.Text) = (this.Ergebnis_Textbox.Text);          //Swap-Button Funktion, value wird gleich dem ergebnis gesetzt
-            this.Ergebnis_Textbox.Text = "";            //Swap-Button Funktion, Ergebnis wird gecleared
+            (this.combobox_fromcurrency.SelectedIndex, this.combobox_tocurrency.SelectedIndex) = (this.combobox_tocurrency.SelectedIndex, this.combobox_fromcurrency.SelectedIndex);            //Swap-Button funktion, tauschen der combobox untereinander 
+            (this.textbox_valuetoconvert.Text) = (this.textbox_ergebnis.Text);          //Swap-Button Funktion, value wird gleich dem ergebnis gesetzt
+            this.textbox_ergebnis.Text = "";            //Swap-Button Funktion, Ergebnis wird gecleared
         }
 
 
@@ -73,29 +74,30 @@ namespace GUI_notconsole_HARD
         {
 
             // Lese den Text String der Textbox aus und tausche . zu ,
-            string replace = Regex.Replace((this.valuetoconvert.Text), @"\.|[a-zA-Z;:_!§$%&/()=?*+'#üäö<>^°´`´ß""-]|[ ]", "");
+            string replace = Regex.Replace((this.textbox_valuetoconvert.Text), @"\.|[a-zA-Z;:_!§$%&/()=?*+'#üäö<>^°´`´ß""-]|[ ]", "");
             // Setze den so bereinigten Text zurück in die Textbox
-            this.valuetoconvert.Text = replace;
+            this.textbox_valuetoconvert.Text = replace;
             //Setze den Courser ans Ende des bereinigten Strings in der Textbox
-            this.valuetoconvert.CaretIndex = replace.Length;
+            this.textbox_valuetoconvert.CaretIndex = replace.Length;
 
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
             // Alles Variablen auf Null setzen
-            this.valuetoconvert.Text = "";
-            this.Ergebnis_Textbox.Text = "";
+            this.textbox_valuetoconvert.Text = "";
+            this.textbox_ergebnis.Text = "";
             this.log_textbox.Text = "";
+            this.textbox_exchangerate.Text = "";
             toCurrency = -1;
             fromCurrency = -1;
-            fromcurrency_combobox.SelectedIndex = 0;
-            tocurrency_combobox.SelectedIndex = 1;
+            combobox_fromcurrency.SelectedIndex = 0;
+            combobox_tocurrency.SelectedIndex = 1;
         }
 
         private void valuetoconvert_GotFocus(object sender, RoutedEventArgs e)
         {
-            this.valuetoconvert.Text = "";
+            this.textbox_valuetoconvert.Text = "";
 
         }
     }
